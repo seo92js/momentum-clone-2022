4cc8330eccf756cc5a720b0639466c3fd2019e44
Seo JeongSeop <seo92js@gmail.com>
2022-08-07 02:11:03 +0900

    Update the weather every 5 minutes

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,9 @@
 const API_KEY = "f0ee5df6040252226a85e99b0aa83ebf";
 
+const city = document.querySelector("#weather span:first-child");
+const weather = document.querySelector("#weather span:nth-child(2)");
+const temp = document.querySelector("#weather span:last-child");
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
@@ -8,10 +12,6 @@ function onGeoOk(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const city = document.querySelector("#weather span:first-child");
-      const weather = document.querySelector("#weather span:nth-child(2)");
-      const temp = document.querySelector("#weather span:last-child");
-
       city.innerText = data.name;
       weather.innerText = data.weather[0].main;
       temp.innerText = data.main.temp;
@@ -19,7 +19,15 @@ function onGeoOk(position) {
 }
 
 function onGeoError() {
-  alert("Can't find you. No weather for you.");
+  city.innerText = "Can't find you. No weather for you.";
+  weather.innerText = "";
+  temp.innerText = "";
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+}
+
+getWeather();
+
+setInterval(getWeather, 300000);
